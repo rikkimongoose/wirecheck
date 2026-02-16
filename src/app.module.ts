eb7a7abeec23392e74a428e57f5108946e8f92f0
Rikki Mongoose <alexander.a.teut@gmail.com>
2026-02-16 04:08:55 +0300

    * fix build

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,32 @@
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { LoggerModule } from 'nestjs-pino';
+
+import appConfig from './config/app.config';
+import mongoConfig from './config/mongo.config';
+
 import { ProjectsModule } from './modules/projects/projects.module';
 import { TestsModule } from './modules/tests/tests.module';
+import { HealthModule } from './modules/health/health.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URL || 'mongodb://localhost:27017/wirecheck'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, mongoConfig],
+    }),
+    LoggerModule.forRoot(),
+
+    MongooseModule.forRootAsync({
+      useFactory: mongoConfig,
+    }),
+
     ProjectsModule,
     TestsModule,
+    HealthModule,
     SeedModule, // <- теперь SeedService доступен
   ],
 })
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from 'nestjs-pino';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
+
+  const config = new DocumentBuilder()
+    .setTitle('Mock Test Generator API')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
